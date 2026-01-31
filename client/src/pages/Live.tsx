17ae462a47007221a676854d4a52757124838a24
rolguergf <rolguer@rogautomacao.net>
2026-01-31 08:07:22 -0300

    Update Live.tsx

--- client/src/pages/Live.tsx
+++ client/src/pages/Live.tsx
@@ -398,7 +398,7 @@ export default function LivePage() {
               <div className="flex-1 rounded-xl border border-slate-800 overflow-hidden shadow-2xl shadow-cyan-500/10 bg-slate-900">
           
                 <iframe
-                  src="https://www.youtube.com/embed/Lx9rmSHl9qw"
+                  src="https://www.youtube.com/embed/NGhh5ZhGOD0"
                   className="w-full h-full"
                   allow="autoplay; encrypted-media; picture-in-picture"
                   allowFullScreen
@@ -701,6 +701,7 @@ export default function LivePage() {
   );
 
 }
+
 
 
 
