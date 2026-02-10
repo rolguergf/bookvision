b820d51142c1663247e2b362679e65df8a7c8d88
rolguergf <rolguer@rogautomacao.net>
2026-02-10 16:33:43 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import Live from "./pages/Live";
 import Cadastro from "./pages/Cadastro";
+import Educacao from "./pages/Educacao";
 
 function Router() {
   return (
@@ -14,6 +15,7 @@ function Router() {
       <Route path={"/"} component={Home} />
       <Route path={"/live"} component={Live} />
       <Route path={"/cadastro"} component={Cadastro} />
+      <Route path={"/educacao"} component={Educacao} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
